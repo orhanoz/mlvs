58977249d53a059142c40c664defc839b639c559
orhanoz <orhanoz91@gmail.com>
2017-01-30 15:34:04 +0200

    indent

--- pure_pursuit.cpp
+++ pure_pursuit.cpp
@@ -6,19 +6,18 @@ PLUGINLIB_DECLARE_CLASS(pure_pursuit, PurePursuit, pure_pursuit::PurePursuit, na
 
 namespace pure_pursuit{
     PurePursuit::PurePursuit(): tf_(NULL), costmap_ros_(NULL) {}
-	
 	//1 todos
-	void PurePursuit::initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros){
-		tf_ = tf;
-		costmap_ros_ = costmap_ros;
-		current_waypoint_ = 0;
-		goal_reached_time_ = ros::Time::now();
-		ros::NodeHandle node_private("~/" + name);
-		collision_planner_.initialize(name, tf_, costmap_ros_);
-		//TODO:paramlar gelicek
-		node_private.param("tolerance_timeout", tolerance_timeout_, 2.5);
-		node_private.param("epsilon", epsilon_, 1e-6);
-		node_private.param("frequency", frequency_, 20.0);
+    void PurePursuit::initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros){
+        tf_ = tf;
+        costmap_ros_ = costmap_ros;
+        current_waypoint_ = 0;
+        goal_reached_time_ = ros::Time::now();
+        ros::NodeHandle node_private("~/" + name);
+        collision_planner_.initialize(name, tf_, costmap_ros_);
+        //TODO:paramlar gelicek
+        node_private.param("tolerance_timeout", tolerance_timeout_, 2.5);
+        node_private.param("epsilon", epsilon_, 1e-6);
+        node_private.param("frequency", frequency_, 20.0);
         node_private.param("initial_waypoint", initialWayPoint_, -1);
         node_private.param("look_ahead_ratio",lookAheadRatio_, 1.0);
         node_private.param("samples", samples_, 100);
@@ -36,20 +35,19 @@ namespace pure_pursuit{
         std::string poseFrameId_;
         int nextWayPoint_;
         double velocity_;
-        
         ROS_DEBUG("Initialized");
-	}
+    }
 	
-	//ok
-	void PurePursuit::odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
-		boost::mutex::scoped_lock lock(odom_lock_);
-		base_odom_.twist.twist.linear.x = msg->twist.twist.linear.x;
-		base_odom_.twist.twist.linear.y = msg->twist.twist.linear.y;
-		base_odom_.twist.twist.angular.z = msg->twist.twist.angular.z;
+    //ok
+    void PurePursuit::odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
+        boost::mutex::scoped_lock lock(odom_lock_);
+        base_odom_.twist.twist.linear.x = msg->twist.twist.linear.x;
+        base_odom_.twist.twist.linear.y = msg->twist.twist.linear.y;
+        base_odom_.twist.twist.angular.z = msg->twist.twist.angular.z;
         currentVelocity_ = msg->twist.twist;
-		ROS_DEBUG("In the odometry callback with velocity values: (%.2f, %.2f, %.2f)",
-		base_odom_.twist.twist.linear.x, base_odom_.twist.twist.linear.y, base_odom_.twist.twist.angular.z);
-	}
+        ROS_DEBUG("In the odometry callback with velocity values: (%.2f, %.2f, %.2f)",
+        base_odom_.twist.twist.linear.x, base_odom_.twist.twist.linear.y, base_odom_.twist.twist.angular.z);
+    }
 	
     //ok
     void PurePursuit::pathCallback(const nav_msgs::Path& msg) {
@@ -57,37 +55,37 @@ namespace pure_pursuit{
         nextWayPoint_ = -1;
     }
     
-	//ok
-	bool PurePursuit::setPlan(const std::vector<geometry_msgs::PoseStamped>& global_plan){
-		current_waypoint_ = 0;
-		goal_reached_time_ = ros::Time::now();
-		if(!transformGlobalPlan(*tf_, global_plan, *costmap_ros_, costmap_ros_->getGlobalFrameID(), global_plan_)){
-			ROS_ERROR("Could not transform the global plan to the frame of the controller");
-			return false;
-		}
-		return true;
-	}
+    //ok
+    bool PurePursuit::setPlan(const std::vector<geometry_msgs::PoseStamped>& global_plan){
+        current_waypoint_ = 0;
+        goal_reached_time_ = ros::Time::now();
+        if(!transformGlobalPlan(*tf_, global_plan, *costmap_ros_, costmap_ros_->getGlobalFrameID(), global_plan_)){
+            ROS_ERROR("Could not transform the global plan to the frame of the controller");
+            return false;
+        }
+        return true;
+    }
 	
-	//ok
-	bool PurePursuit::isGoalReached(){
-		if(goal_reached_time_ + ros::Duration(tolerance_timeout_) < ros::Time::now() && stopped()){
-			return true;
-		}
-		return false;
-	}
+    //ok
+    bool PurePursuit::isGoalReached(){
+        if(goal_reached_time_ + ros::Duration(tolerance_timeout_) < ros::Time::now() && stopped()){
+            return true;
+        }
+        return false;
+    }
 	
-	//ok
-	bool PurePursuit::stopped(){
-		//copy over the odometry information
-		nav_msgs::Odometry base_odom;
-		{
-		boost::mutex::scoped_lock lock(odom_lock_);
-		base_odom = base_odom_;
-		}
-		return fabs(base_odom.twist.twist.angular.z) <= rot_stopped_velocity_
-		&& fabs(base_odom.twist.twist.linear.x) <= trans_stopped_velocity_
-		&& fabs(base_odom.twist.twist.linear.y) <= trans_stopped_velocity_;
-	}
+    //ok
+    bool PurePursuit::stopped(){
+        //copy over the odometry information
+        nav_msgs::Odometry base_odom;
+        {
+        boost::mutex::scoped_lock lock(odom_lock_);
+        base_odom = base_odom_;
+        }
+        return fabs(base_odom.twist.twist.angular.z) <= rot_stopped_velocity_
+        && fabs(base_odom.twist.twist.linear.x) <= trans_stopped_velocity_
+        && fabs(base_odom.twist.twist.linear.y) <= trans_stopped_velocity_;
+    }
 	
     geometry_msgs::PoseStamped PurePursuit::getCurrentPose(){
         geometry_msgs::PoseStamped pose, transformedPose;
@@ -103,39 +101,32 @@ namespace pure_pursuit{
         return transformedPose;
     }
     
-	//1 todo		
-	bool PurePursuit::computeVelocityCommands(geometry_msgs::Twist& cmd_vel){
-		//get the current pose of the robot in the fixed frame
-		tf::Stamped<tf::Pose> robot_pose;
-		if(!costmap_ros_->getRobotPose(robot_pose)){
-		ROS_ERROR("Can't get robot pose");
-		geometry_msgs::Twist empty_twist;
-		cmd_vel = empty_twist;
-		return false;
-		}
+    //1 todo		
+    bool PurePursuit::computeVelocityCommands(geometry_msgs::Twist& cmd_vel){
+        //get the current pose of the robot in the fixed frame
+        tf::Stamped<tf::Pose> robot_pose;
+        if(!costmap_ros_->getRobotPose(robot_pose)){
+            ROS_ERROR("Can't get robot pose");
+            geometry_msgs::Twist empty_twist;
+            cmd_vel = empty_twist;
+            return false;
+        }
 		//waypoint hangisi hangisi ?
-		tf::Stamped<tf::Pose> target_pose;
-		tf::Stamped<tf::Pose> cur_pose_of_waypoints;
-		tf::Stamped<tf::Pose> cur_pose_of_waypoints_next;
-		tf::poseStampedMsgToTF(global_plan_[current_waypoint_], cur_pose_of_waypoints);
+        tf::Stamped<tf::Pose> target_pose;
+        tf::Stamped<tf::Pose> cur_pose_of_waypoints;
+        tf::Stamped<tf::Pose> cur_pose_of_waypoints_next;
+        tf::poseStampedMsgToTF(global_plan_[current_waypoint_], cur_pose_of_waypoints);
     
-		//lookahead angle,distance
-		tf::Vector3 lookAheadDistance_ = getLookAheadDistance(cur_pose_of_waypoints, robot_pose);
-		tf::Vector3 lookAheadAngle_ = getLookAheadAngle(cur_pose_of_waypoints, robot_pose);
-		//arc distance
-		double arcDistance=getArcDistance(lookAheadDistance_, lookAheadAngle_);
+        //lookahead angle,distance
+        tf::Vector3 lookAheadDistance_ = getLookAheadDistance(cur_pose_of_waypoints, robot_pose);
+        tf::Vector3 lookAheadAngle_ = getLookAheadAngle(cur_pose_of_waypoints, robot_pose);
+        //arc distance
+        double arcDistance=getArcDistance(lookAheadDistance_, lookAheadAngle_);
         
         ROS_DEBUG("PurePursuit: current robot pose %f %f ==> %f", robot_pose.getOrigin().x(), robot_pose.getOrigin().y(), tf::getYaw(robot_pose.getRotation()));
         ROS_DEBUG("PurePursuit: target robot pose %f %f ==> %f", target_pose.getOrigin().x(), target_pose.getOrigin().y(), tf::getYaw(target_pose.getRotation()));
-        
-        //curvature=(2*x)/(arcDistance)^2
-        
-        
-		//TODO:arcDistance to vel command nasil cikar?
-        
-        
-        
-        
+
+        //TODO:arcDistance to vel command nasil cikar?
         
         //if we're not in the goal position, we need to update time
         if(!in_goal_position)
@@ -147,34 +138,34 @@ namespace pure_pursuit{
             cmd_vel = empty_twist;
         }
         return true;
-	}
+    }
 
     //ok
     double PurePursuit::getLookAheadThreshold(){
         return lookAheadRatio_*currentVelocity_.linear.x;
     }
 
-	//ok sıkıntı olabilir, tf::poseda pose.position
-	double::PurePursuit::getLookAheadDistance(const tf::Pose& pose1, const tf::Pose& pose2){
-		tf::Vector3 v1(pose2.pose.position.x,pose2.pose.position.y,pose2.pose.position.z);
-		tf::Vector3 v2(pose1.pose.position.x,pose1.pose.position.y,pose1.pose.position.z);
-		return tf::tfDistance(v1, v2);
-	}
+    //ok sıkıntı olabilir, tf::poseda pose.position
+    double::PurePursuit::getLookAheadDistance(const tf::Pose& pose1, const tf::Pose& pose2){
+        tf::Vector3 v1(pose2.pose.position.x,pose2.pose.position.y,pose2.pose.position.z);
+        tf::Vector3 v2(pose1.pose.position.x,pose1.pose.position.y,pose1.pose.position.z);
+        return tf::tfDistance(v1, v2);
+    }
 	
-	//ok sıkıntı olabilir, tf::poseda pose.position 
-	double PurePursuit::getLookAheadAngle(const tf::Pose& pose1, const tf::Pose& pose2){
-		tf::Vector3 v1(pose2.pose.position.x,pose2.pose.position.y,pose2.pose.position.z);
-		tf::Vector3 v2(pose1.pose.position.x,pose1.pose.position.y,pose1.pose.position.z);
-		return tf::tfAngle(v1, v2);
-	}
+    //ok sıkıntı olabilir, tf::poseda pose.position 
+    double PurePursuit::getLookAheadAngle(const tf::Pose& pose1, const tf::Pose& pose2){
+        tf::Vector3 v1(pose2.pose.position.x,pose2.pose.position.y,pose2.pose.position.z);
+        tf::Vector3 v2(pose1.pose.position.x,pose1.pose.position.y,pose1.pose.position.z);
+        return tf::tfAngle(v1, v2);
+    }
 	
-	//ok parametreler doubleda olablü bakmak lazım
-	double PurePursuit::getArcDistance(const tf::Vector3& lookAheadDistance,const tf::Vector3& lookAheadAngle){
-		if (std::abs(std::sin(lookAheadAngle)) >= epsilon_)
-			return lookAheadDistance/sin(lookAheadAngle)*lookAheadAngle;
-		else
-			return lookAheadDistance;
-	}
+    //ok parametreler doubleda olablü bakmak lazım
+    double PurePursuit::getArcDistance(const tf::Vector3& lookAheadDistance,const tf::Vector3& lookAheadAngle){
+        if (std::abs(std::sin(lookAheadAngle)) >= epsilon_)
+            return lookAheadDistance/sin(lookAheadAngle)*lookAheadAngle;
+        else
+            return lookAheadDistance;
+    }
 
     //yeni eklendi
     int PurePursuit::getNextWayPoint(int wayPoint){   
@@ -185,7 +176,7 @@ namespace pure_pursuit{
                         origin.pose.position.y,
                         origin.pose.position.z);
                 double lookAheadThreshold = getLookAheadThreshold();
-
+                
                 for (int i = nextWayPoint_; i < currentReferencePath_.poses.size();++i) {                
                     tf::Vector3 v_2(currentReferencePath_.poses[i].pose.position.x,
                           currentReferencePath_.poses[i].pose.position.y,
@@ -195,7 +186,7 @@ namespace pure_pursuit{
                         return i;
                 }
 
-            return nextWayPoint_;
+                return nextWayPoint_;
             }
             else
                 return 0;
@@ -205,100 +196,95 @@ namespace pure_pursuit{
 
     //getinter
     bool PurePursuit::getInterpolatedPose(int wayPoint,
-      geometry_msgs::PoseStamped& interpolatedPose){
+        geometry_msgs::PoseStamped& interpolatedPose){
         if (!currentReferencePath_.poses.empty()) {
             if (wayPoint > 0) {
                 double l_t = getLookAheadThreshold();
                 double p_t = getLookAheadDistance(currentReferencePath_.poses[nextWayPoint_-1]);
         
-        if (p_t < l_t) {
-          geometry_msgs::PoseStamped p_0 = getCurrentPose();
-          geometry_msgs::PoseStamped p_1 = 
-            currentReferencePath_.poses[wayPoint-1];
-          geometry_msgs::PoseStamped p_2 = 
-            currentReferencePath_.poses[wayPoint];
+                if (p_t < l_t) {
+                    geometry_msgs::PoseStamped p_0 = getCurrentPose();
+                    geometry_msgs::PoseStamped p_1 = 
+                    currentReferencePath_.poses[wayPoint-1];
+                    geometry_msgs::PoseStamped p_2 = 
+                    currentReferencePath_.poses[wayPoint];
           
-          tf::Vector3 v_1(p_2.pose.position.x-p_0.pose.position.x,
+                    tf::Vector3 v_1(p_2.pose.position.x-p_0.pose.position.x,
                           p_2.pose.position.y-p_0.pose.position.y,
                           p_2.pose.position.z-p_0.pose.position.z);
-          tf::Vector3 v_2(p_1.pose.position.x-p_0.pose.position.x,
+                    tf::Vector3 v_2(p_1.pose.position.x-p_0.pose.position.x,
                           p_1.pose.position.y-p_0.pose.position.y,
                           p_1.pose.position.z-p_0.pose.position.z);
-          tf::Vector3 v_0(p_2.pose.position.x-p_1.pose.position.x,
+                    tf::Vector3 v_0(p_2.pose.position.x-p_1.pose.position.x,
                           p_2.pose.position.y-p_1.pose.position.y,
                           p_2.pose.position.z-p_1.pose.position.z);
           
-          double l_0 = v_0.length();
-          double l_1 = v_1.length();
-          double l_2 = v_2.length();
-          
-          v_0.normalize();
-          v_2.normalize();
-          
-          double alpha_1 = M_PI-tf::tfAngle(v_0, v_2);
-          double beta_2 = asin(l_2*sin(alpha_1)/l_t);
-          double beta_0 = M_PI-alpha_1-beta_2;
-          double l_s = l_2*sin(beta_0)/sin(beta_2);
-          tf::Vector3 p_s(p_1.pose.position.x+v_0[0]*l_s,
+                    double l_0 = v_0.length();
+                    double l_1 = v_1.length();
+                    double l_2 = v_2.length();
+                    v_0.normalize();
+                    v_2.normalize();
+                    
+                    double alpha_1 = M_PI-tf::tfAngle(v_0, v_2);
+                    double beta_2 = asin(l_2*sin(alpha_1)/l_t);
+                    double beta_0 = M_PI-alpha_1-beta_2;
+                    double l_s = l_2*sin(beta_0)/sin(beta_2);
+                    tf::Vector3 p_s(p_1.pose.position.x+v_0[0]*l_s,
                           p_1.pose.position.x+v_0[1]*l_s,
                           p_1.pose.position.x+v_0[2]*l_s);
-
-          interpolatedPose.pose.position.x = p_s[0];
-          interpolatedPose.pose.position.y = p_s[1];
-          interpolatedPose.pose.position.z = p_s[2];
+                          
+                    interpolatedPose.pose.position.x = p_s[0];
+                    interpolatedPose.pose.position.y = p_s[1];
+                    interpolatedPose.pose.position.z = p_s[2];
           
-          return true;
+                    return true;
+                }
+            }
+            interpolatedPose = currentReferencePath_.poses[wayPoint];
+            return true;
         }
-      }
-       interpolatedPose = currentReferencePath_.poses[wayPoint];
-      return true;
-    }
-      
-    return false;
+        return false;
     }
 
     //step
     bool PurePursuit::step(geometry_msgs::Twist& twist) {
-    twist.linear.x = 0.0;
-    twist.linear.y = 0.0;
-    twist.linear.z = 0.0;
+        twist.linear.x = 0.0;
+        twist.linear.y = 0.0;
+        twist.linear.z = 0.0;
     
-    twist.angular.x = 0.0;
-    twist.angular.y = 0.0;
-    twist.angular.z = 0.0;
+        twist.angular.x = 0.0;
+        twist.angular.y = 0.0;
+        twist.angular.z = 0.0;
     
-    nextWayPoint_ = getNextWayPoint(nextWayPoint_);
+        nextWayPoint_ = getNextWayPoint(nextWayPoint_);
 
-    if (nextWayPoint_ >= 0) {
-      geometry_msgs::PoseStamped pose;
+        if (nextWayPoint_ >= 0) {
+            geometry_msgs::PoseStamped pose;
       
-      if (getInterpolatedPose(nextWayPoint_, pose)) {
-        double lookAheadDistance = getLookAheadDistance(pose);
-        double lookAheadAngle = getLookAheadAngle(pose);
+            if (getInterpolatedPose(nextWayPoint_, pose)) {
+                double lookAheadDistance = getLookAheadDistance(pose);
+                double lookAheadAngle = getLookAheadAngle(pose);
 
-        double angularVelocity = 0.0;
-        if (std::abs(std::sin(lookAheadAngle)) >= epsilon_) {
-          double radius = 0.5*(lookAheadDistance/std::sin(lookAheadAngle));
-          
-          double linearVelocity = velocity_;
-          if (std::abs(radius) >= epsilon_)
-            angularVelocity = linearVelocity/radius;
+                double angularVelocity = 0.0;
+                if (std::abs(std::sin(lookAheadAngle)) >= epsilon_) {
+                    double radius = 0.5*(lookAheadDistance/std::sin(lookAheadAngle));
+                    
+                    double linearVelocity = velocity_;
+                    if (std::abs(radius) >= epsilon_)
+                        angularVelocity = linearVelocity/radius;
 
-          twist.linear.x = linearVelocity;
-          twist.angular.z = angularVelocity;
-          
-          return true;
+                    twist.linear.x = linearVelocity;
+                    twist.angular.z = angularVelocity;
+                    return true;
+                }
+            }
         }
-      }
+        return false;
     }
-    
-    return false;
-  }
-
      
     
     //ok -- cozuldu 
-	bool PurePursuit::transformGlobalPlan(const tf::TransformListener& tf, const std::vector<geometry_msgs::PoseStamped>& global_plan, 
+    bool PurePursuit::transformGlobalPlan(const tf::TransformListener& tf, const std::vector<geometry_msgs::PoseStamped>& global_plan, 
 		const costmap_2d::Costmap2DROS& costmap, const std::string& global_frame,
 		std::vector<geometry_msgs::PoseStamped>& transformed_plan){
 			
