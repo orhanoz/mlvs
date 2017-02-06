6fd6dbb33845f6782959d0a03db4240af84ec101
orhanoz <orhanoz91@gmail.com>
2017-02-06 16:55:08 +0200

    latest

--- pure_pursuit/include/pure_pursuit/pure_pursuit.h
+++ pure_pursuit/include/pure_pursuit/pure_pursuit.h
@@ -21,27 +21,30 @@ namespace pure_pursuit {
             bool isGoalReached();
             bool setPlan(const std::vector<geometry_msgs::PoseStamped>& global_plan);
             bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
-            double getLookAheadThreshold();
-            double getLookAheadDistance(const geometry_msgs::PoseStamped& pose1);
-            double getLookAheadAngle(const geometry_msgs::PoseStamped& pose1);
-            double getArcDistance(double lookAheadDistance,double lookAheadAngle);
-            int getNextWayPoint(int wayPoint);
+            //int getNextWayPoint(int wayPoint);
             bool getInterpolatedPose(int wayPoint,geometry_msgs::PoseStamped& interpolatedPose);
-            bool step(geometry_msgs::Twist& twist);
+            //bool step(geometry_msgs::Twist& twist);
+            //bool sendCmdVel(geometry_msgs::Twist& cmd_vel);
+            double getLookAheadThreshold();
+            double getLookAheadDistance(int waypoint);
+            double getLookAheadAngle(int waypoint);
+            double getArcDistance(int waypoint);
             
         private:
             inline double sign(double n){
                 return n < 0.0 ? -1.0 : 1.0;
             }
-      
+        
+        geometry_msgs::Twist limitTwist(const geometry_msgs::Twist& twist);
         geometry_msgs::PoseStamped getCurrentPose();
         bool transformGlobalPlan(const tf::TransformListener& tf, 
             const std::vector<geometry_msgs::PoseStamped>& global_plan, 
             const costmap_2d::Costmap2DROS& costmap, const std::string& global_frame,
             std::vector<geometry_msgs::PoseStamped>& transformed_plan);
         void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
-        void pathCallback(const nav_msgs::Path::ConstPtr& msg);
         bool stopped();
+        double headingDiff(double x, double y, double pt_x, double pt_y, double heading);
+        geometry_msgs::Twist diff2D(const tf::Pose& pose1, const tf::Pose& pose2);
       
         tf::TransformListener* tf_;
         tf::TransformListener tfListener_;
@@ -60,13 +63,14 @@ namespace pure_pursuit {
         double min_vel_lin_, min_vel_th_;
         double min_in_place_vel_th_, in_place_trans_vel_;
         bool holonomic_;
+        
         boost::mutex odom_lock_;
         ros::Subscriber odom_sub_;
         nav_msgs::Odometry base_odom_;
 
-        boost::mutex path_lock_;
-        ros::Subscriber path_sub_;
-        nav_msgs::Path currentReferencePath_;
+        //boost::mutex path_lock_;
+        //ros::Subscriber path_sub_;
+        //nav_msgs::Path currentReferencePath_;
         
         geometry_msgs::Twist currentVelocity_;
         std::string poseFrameId_;
@@ -76,6 +80,9 @@ namespace pure_pursuit {
         base_local_planner::TrajectoryPlannerROS collision_planner_;
         ros::Time goal_reached_time_;
         
+        double max_heading_diff_before_moving_;
+        bool turn_in_place_first_,allow_backwards_;
+        double arcDistance_;
         double trans_stopped_velocity_, rot_stopped_velocity_;
         unsigned int current_waypoint_; 
         int samples_,nextWayPoint_,initialWayPoint_;
